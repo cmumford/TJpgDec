91c7d48adcaee1aab8a68ddbfd439a67ca454697
Chris Mumford <cmumford@cmumford.com>
2021-01-28 17:26:19 -0800

    test: Added some error checking.

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@
 .PHONY: format
 format:
 	clang-format -i test/simple/*.c
+
+.PHONY: test
+test:
+	build/simple test/images
--- test/simple/simple.c
+++ test/simple/simple.c
@@ -12,6 +12,10 @@
                  */
 #define SCALE 0 /* Output scaling 0:1/1, 1:1/2, 2:1/4 or 3:1/8 */
 
+#if !defined(MAX_PATH)
+#define MAX_PATH 256
+#endif
+
 /* User defined session identifier */
 
 typedef struct {
@@ -99,10 +103,10 @@ int output_func(              /* 1:Ok, 0:Aborted */
   return 1; /* Continue to decompress */
 }
 
-void write_bmp(const char* fname,
-               uint8_t* buf,
-               uint32_t width,
-               uint32_t height) {
+int write_bmp(const char* fname,
+              uint8_t* buf,
+              uint32_t width,
+              uint32_t height) {
   uint32_t i, xb = (width * 3 + 3) & ~3;
   uint8_t *s, *d, r, g, b;
   FILE* hbmp;
@@ -110,7 +114,7 @@ void write_bmp(const char* fname,
 
   hbmp = fopen(fname, "w");
   if (!hbmp)
-    return;
+    return errno;
 
   BMP_FILEHEADER bfh;
   memset(&bfh, 0, sizeof bfh);
@@ -118,6 +122,11 @@ void write_bmp(const char* fname,
   bfh.size = sizeof(BMP_FILEHEADER) + sizeof(BMP_INFOHEADER) + xb * height;
   bfh.offset = sizeof(BMP_FILEHEADER) + sizeof(BMP_INFOHEADER);
   written = fwrite(&bfh, 1, sizeof(bfh), hbmp);
+  if (written != sizeof(bfh)) {
+    int err = errno;
+    fclose(hbmp);
+    return err;
+  }
 
   BMP_INFOHEADER bih;
   memset(&bih, 0, sizeof bih);
@@ -128,6 +137,11 @@ void write_bmp(const char* fname,
   bih.bits = 24;
   bih.compression = COMPRESSON_RGB;
   written = fwrite(&bih, 1, sizeof(bih), hbmp);
+  if (written != sizeof(bih)) {
+    int err = errno;
+    fclose(hbmp);
+    return err;
+  }
 
   /* Flip top and down, swap byte order RGB to BGR */
   s = buf;
@@ -148,12 +162,18 @@ void write_bmp(const char* fname,
     s += xb;
   }
 
-  written = fwrite(buf, height, xb, hbmp);
+  int err = 0;
+  size_t to_write = height * xb;
+  written = fwrite(buf, 1, to_write, hbmp);
+  if (written != to_write)
+    err = errno;
 
   fclose(hbmp);
+
+  return err;
 }
 
-void jpegtest(char* fn) {
+int jpegtest(char* fn) {
   const size_t sz_work = 4096; /* Size of working buffer for TJpgDec module */
   void* jdwork;                /* Pointer to TJpgDec work area */
   JDEC jd;                     /* TJpgDec decompression object */
@@ -167,77 +187,92 @@ void jpegtest(char* fn) {
 
   /* Open JPEG file */
   iodev.hin = fopen(fn, "r");
-  if (iodev.hin) {
-    jdwork = calloc(1, sz_work);
+  if (!iodev.hin)
+    return errno;
 
-    rc = jd_prepare(&jd, input_func, jdwork, (unsigned int)sz_work,
-                    &iodev); /* Prepare to decompress the file */
-
-    if (!rc) {
-      if (MODE == 1) {                         /* Put file properties */
-        printf(",%u,%u", jd.width, jd.height); /* Image size */
-        printf(",[%d:%d:%d]", 4, 4 / jd.msx,
-               (jd.msy == 2) ? 0 : (jd.msx == 1) ? 4 : 2); /* Sampling ratio */
-        printf(
-            ",%lu",
-            sz_work -
-                jd.sz_pool); /* Get used memory size by rest of memory pool */
-      } else {
-        printf("\n");
-      }
-      xs = jd.width >> SCALE; /* Output size */
-      ys = jd.height >> SCALE;
-      xb = (xs * 3 + 3) & ~3; /* Byte width of the frame buffer */
-      iodev.wbyte = xb;
-      iodev.frmbuf = malloc(xb * ys); /* Allocate an output frame buffer */
-      rc = jd_decomp(&jd, output_func, SCALE); /* Start to decompress */
-      if (!rc) { /* Save the decompressed picture as a bmp file */
-        if (MODE == 1) {
-          printf(",%d", rc);
-        } else {
-          printf("\rOK  ");
-          strcpy(str, fn);
-          strcpy(str + strlen(str) - 4, ".bmp");
-          write_bmp(str, (uint8_t*)iodev.frmbuf, xs, ys);
-        }
-      } else { /* Error occured on decompress */
-        if (MODE == 1) {
-          printf(",%d", rc);
-        } else {
-          printf("\rError(%d)", rc);
-        }
-      }
-      free(iodev.frmbuf); /* Discard frame buffer */
-    } else {              /* Error occured on prepare */
-      if (MODE == 1) {
-        printf(",,,,,%d", rc);
-      } else {
-        printf("\nErr: %d", rc);
-      }
-    }
-
-    free(jdwork); /* Discard work area */
-
-    fclose(iodev.hin); /* Close JPEG file */
+  jdwork = calloc(1, sz_work);
+  if (!jdwork) {
+    fclose(iodev.hin);
+    return ENOMEM;
   }
 
+  rc = jd_prepare(&jd, input_func, jdwork, (unsigned int)sz_work,
+                  &iodev); /* Prepare to decompress the file */
+
+  if (!rc) {
+    if (MODE == 1) {                         /* Put file properties */
+      printf(",%u,%u", jd.width, jd.height); /* Image size */
+      printf(",[%d:%d:%d]", 4, 4 / jd.msx,
+             (jd.msy == 2) ? 0 : (jd.msx == 1) ? 4 : 2); /* Sampling ratio */
+      printf(",%lu",
+             sz_work -
+                 jd.sz_pool); /* Get used memory size by rest of memory pool */
+    } else {
+      printf("\n");
+    }
+    xs = jd.width >> SCALE; /* Output size */
+    ys = jd.height >> SCALE;
+    xb = (xs * 3 + 3) & ~3; /* Byte width of the frame buffer */
+    iodev.wbyte = xb;
+    iodev.frmbuf = malloc(xb * ys); /* Allocate an output frame buffer */
+    rc = jd_decomp(&jd, output_func, SCALE); /* Start to decompress */
+    if (!rc) { /* Save the decompressed picture as a bmp file */
+      if (MODE == 1) {
+        printf(",%d", rc);
+      } else {
+        printf("\rOK  ");
+        strcpy(str, fn);
+        strcpy(str + strlen(str) - 4, ".bmp");
+        rc = write_bmp(str, (uint8_t*)iodev.frmbuf, xs, ys);
+      }
+    } else { /* Error occured on decompress */
+      if (MODE == 1) {
+        printf(",%d", rc);
+      } else {
+        printf("\rError(%d)", rc);
+      }
+    }
+    free(iodev.frmbuf); /* Discard frame buffer */
+  } else {              /* Error occured on prepare */
+    if (MODE == 1) {
+      printf(",,,,,%d", rc);
+    } else {
+      printf("\nErr: %d", rc);
+    }
+  }
+
+  free(jdwork); /* Discard work area */
+
+  fclose(iodev.hin); /* Close JPEG file */
+
   printf("\n");
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "\n\nusage:\n\n");
+    fprintf(stderr, "simple <image_dir>\n\n");
+    return ENOENT;
+  }
+
   if (MODE == 1)
     printf("File Name,Width,Height,Sampling,Used Memory,Result\n");
 
-  DIR* d = opendir(".");
+  DIR* d = opendir(argv[1]);
   if (!d) {
-    fprintf(stderr, "Cannot open directory\n");
+    fprintf(stderr, "Cannot open \"%s\"\n", argv[1]);
     return errno;
   }
 
   struct dirent* dir;
   while ((dir = readdir(d))) {
-    if (strstr(dir->d_name, ".jpg") || strstr(dir->d_name, ".JPG"))
-      jpegtest(dir->d_name);
+    if (strstr(dir->d_name, ".jpg") || strstr(dir->d_name, ".JPG")) {
+      char file_name[MAX_PATH];
+      if (snprintf(file_name, MAX_PATH, "%s/%s", argv[1], dir->d_name) > 0) {
+        jpegtest(file_name);
+      }
+    }
   }
 
   return 0;
